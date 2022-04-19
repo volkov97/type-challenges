73ff3f96599c5996b2058138ec43ab707eb8d883
masx200 <34191203+masx200@users.noreply.github.com>
2022-04-19 16:54:39 +0000

    feat(question): add #8532 - Array Of Length

---
+++ questions/08532-hard-array-of-length/test-cases.ts
@@ -0,0 +1,6 @@
+import { Equal, Expect, ExpectFalse, NotEqual,ExpectExtends } from '@type-challenges/utils'
+
+type cases = [
+  Expect<ExpectExtends<ArrayOfLength<number, 4> , [number, number, number, number]>>,
+  Expect<ExpectExtends<[number, number, number, number],ArrayOfLength<number, 4>  >>
+]
