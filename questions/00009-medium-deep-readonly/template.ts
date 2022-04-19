7d791935464ac6c7c236dea3650cf39ce1258001
German Volkov <volkov97@yandex-team.ru>
2022-04-19 23:44:10 +0300

    easy and some first medium tasks

--- questions/00009-medium-deep-readonly/template.ts
+++ questions/00009-medium-deep-readonly/template.ts
@@ -1,1 +1,21 @@
-type DeepReadonly<T> = any
+// type DeepReadonly<T> = {
+//   readonly [P in keyof T]: T[P] extends Record<string, unknown>
+//     ? DeepReadonly<T[P]>
+//     : T[P];
+// }
+
+// type DeepReadonly<T> = {
+//     readonly [P in keyof T]: keyof T[P] extends object ? T[P] : DeepReadonly<T[P]>;
+//   };
+
+type DeepReadonly<T> = keyof T extends never ? T : {
+  readonly [P in keyof T]: DeepReadonly<T[P]>
+}
+
+// type DeepReadonly<T> = {
+//   readonly [P in keyof T]: T[P] extends Record<string, unknown>
+//     ? DeepReadonly<T[P]>
+//     : T[P] extends any[] ?
+//       DeepReadonly<T[P]> :
+//       T[P];
+// }
